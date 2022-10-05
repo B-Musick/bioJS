5b926070cfbc2aedee872ebe68fb7cb1c3f0f7ac
B-Musick <bmuze1@gmail.com>
2022-10-05 16:08:58 -0500

    First commit, can read in fasta file and it will split up the separaate genome sequences into their own objects containing an ID and the sequence string.

---
+++ README.md
@@ -0,0 +1,14 @@
+# FASTA
+- Starts with '>'
+- ';' means comment
+- Blank line after end of sequence
+- First line contains the information
+
+# Sources
+
+## Local Files
+/Users/bmuze1/Desktop/COMPUTER_DESKTOP_2021_AND_BEFORE/WEB_DEVELOPMENT/PROJECTS/BIOINFORMATICS_JS
+
+## Websites
+https://en.wikipedia.org/wiki/FASTA_format 
+https://bioperl.org/formats/sequence_formats/FASTA_sequence_format 
---
+++ bioJS.js
@@ -0,0 +1,62 @@
+// GET FILE INPUT
+let inputElement = document.getElementById("fileItem"); // File input
+let fileInput; // If this has a value then use it
+let codeButtons = document.querySelectorAll('.code-button');
+
+// Method which is called on the class instance to listen for users input 
+inputElement.addEventListener("change", (e) => {
+    // If user wants to input from file instead of to genomeText input 
+    var file = document.getElementById('fileItem').files[0]; // Get the file input
+
+    var reader = new FileReader(); // Read the file
+    reader.readAsText(file); // Read the file as text
+    
+    reader.onload = function () {
+        // Reload the page
+        fileInput = reader.result;
+
+        let extractedGenomes = extractGenomeSequenceObjects(fileInput);
+        console.log(extractedGenomes[0]);
+   
+        // bioinformatics.listen();
+    }
+});
+
+let extractGenomeSequenceObjects = (fileInput) =>{
+    let extractedFastaObjects = [];
+
+    let genomeIdFullRegex = /^>[a-z].*/;
+    // This will match the id value "gi|2765658|emb|Z78533.1|CIZ78533"
+    let genomeIdRegex = /[a-z][A-Za-z0-9|.]*[^\s]/;
+
+    let genomeRegex = /^[A-Z]/i;
+    let fileArray = fileInput.split('\n');
+
+    // If match the genome ID, then the next lines will be the genome until
+    // reach an empty line
+    let genomeObject = {}; // Hold the genome object
+    let newGenome = false;
+    let genomeString = "";
+
+    fileArray.forEach(line => {
+        if (!newGenome && genomeIdFullRegex.test(line)) {
+            genomeObject = {}; // Create new object
+            genomeString = "";
+
+            // If it is the header
+            newGenome = true; // New genome object is started
+            genomeObject.id = line.match(genomeIdRegex)[0];
+        }
+        else if (genomeRegex.test(line)) {
+            // If it is a genome, then add it to the genome string
+            genomeString += line;
+
+        } else if (!line) {
+            // If a blank line, then the genome object has been created
+            genomeObject.sequence = genomeString;
+            newGenome == false;
+            extractedFastaObjects.push(genomeObject);
+        }
+    });
+    return extractedFastaObjects;
+}
